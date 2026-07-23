f40349cc75bd27a36639f21b2b8d7e1c93eaad17
HailuBen <b.hailu.pk@gmail.com>
2026-07-23 13:35:59 -0400

    update display function, add JSON string

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+// part1
 const student = {
     name: "Brean",
     age: 22, 
     enrolled: true, 
     courses: ["Math", "Science", "Geography"],
-    displayInfo(){
-        return name+" is "+age+" years old.";
+    displayInfo() {
+        return this.name+" is "+this.age+" years old.";
     }
 };
+
+console.log(student.name+", "+student.age);
+console.log(student.displayInfo());
+
+//part2
+const studentJSON = JSON.stringify(student);
+console.log(studentJSON);
+
+const studentObject = JSON.parse(studentJSON);
+console.log(studentObject);
+
+console.log(studentObject.name === student.name);
+console.log(studentObject === student);
+
