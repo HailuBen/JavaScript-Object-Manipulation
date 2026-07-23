a8f4793d2c7834b8f92d9cc2bb250e11316e1b23
HailuBen <b.hailu.pk@gmail.com>
2026-07-23 13:25:55 -0400

    add script file with student object

---
+++ script.js
@@ -0,0 +1,9 @@
+const student = {
+    name: "Brean",
+    age: 22, 
+    enrolled: true, 
+    courses: ["Math", "Science", "Geography"],
+    displayInfo(){
+        return name+" is "+age+" years old.";
+    }
+};
